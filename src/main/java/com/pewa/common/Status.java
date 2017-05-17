c614ec42ca25febc6f4475a29282a28627cc6bde
phonik <phonik83@gmail.com>
2017-05-17 18:00:51 +0200

    cos tam

---
+++ src/main/java/com/pewa/common/Status.java
@@ -0,0 +1,7 @@
+package com.pewa.common;
+
+/**
+ * Created by phonik on 2017-05-10.
+ */
+public class Status {
+}
---
+++ src/main/java/com/pewa/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.pewa.controller;
+
+/**
+ * Created by phonik on 2017-05-10.
+ */
+public class IndexController {
+}
